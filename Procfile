e61fbe3a8b2fa9933aa17d3add19a4bdf9d319f6
site4everyone <68636587+site4everyone@users.noreply.github.com>
2022-09-20 22:44:03 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+release:python manage.py migrate
+web:guicorn programming_bot.wsgi --log-file-
--- programming_bot/settings.py
+++ programming_bot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y!+4botf^r)wxdcm*u&+!jlt=ngt+kc5i+w&xj-juz3^=1yc8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['program-bot.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
