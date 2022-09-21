61f10fd15138a2c0aee3f935e8a8641a42e7aaf2
site4everyone <68636587+site4everyone@users.noreply.github.com>
2022-09-21 07:42:40 +0530

    Update settings.py

--- programming_bot/settings.py
+++ programming_bot/settings.py
@@ -49,7 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware'  # crsf_token_middleware
+    'django.middleware.csrf.CsrfViewMiddleware',  # crsf_token_middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'programming_bot.urls'
