00474c0ac3f565b27586e23f109fc2d32961a658
site4everyone <68636587+site4everyone@users.noreply.github.com>
2022-09-21 08:07:40 +0530

    Update models.py

--- uploader/models.py
+++ uploader/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 
 class Problem(models.Model):
+    title = models.CharField(150)
     question = models.TextField()
     solution = models.TextField()
+    author = models.CharField(200)
+    
